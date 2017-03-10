18133ccce849b82cb4ac4e8c42e10bdd3aec29a2
Borna2 <bornagrgic1@gmail.com>
2017-03-10 13:21:51 +0100

    Initial commit

---
+++ app/src/main/java/com/example/borna2/iorder/FragmentPortrait.java
@@ -0,0 +1,19 @@
+package com.example.borna2.iorder;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class FragmentPortrait extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_portrait, container, false);
+    }
+}
